ce9668866ad1b4906356be8cf340a9a49fc74ab3
SulaMabuza <45216771+SulaMabuza@users.noreply.github.com>
2019-01-18 23:38:04 +0000

    Add files via upload

---
+++ lab1.java
@@ -0,0 +1,17 @@
+package ICP;
+
+public class lab1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String input;
+		input = "Ashesi University Intermediat Computer Programming";
+		int strLength = input.length();
+		System.out.println(strLength);
+		
+
+	}
+	
+
+}
